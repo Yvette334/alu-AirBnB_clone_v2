1545ea8a9b7e4f7528780b8d48436c89c1458259
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:30:32 +0200

    add

--- models/place.py
+++ models/place.py
@@ -4,7 +4,15 @@ Contains the Place class
 """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import models
+
+# Association table for Many-to-Many relationship between Place and Amenity
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -21,3 +29,25 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    # For DBStorage: relationship with Amenity using place_amenity as secondary table
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+
+    # For FileStorage: getter and setter for amenities
+    @property
+    def amenities(self):
+        """Getter for amenities in FileStorage"""
+        if models.storage_t != "db":
+            amenity_list = []
+            all_amenities = models.storage.all("Amenity")
+            for amenity in all_amenities.values():
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+        
+    @amenities.setter
+    def amenities(self, obj):
+        """Setter for amenities in FileStorage"""
+        if models.storage_t != "db":
+            if obj.__class__.__name__ == "Amenity":
+                self.amenity_ids.append(obj.id)
