bc3df72de9c2e0b69f5fd6a68ff76199703c6192
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 19:47:02 +0200

    add

--- models/amenity.py
+++ models/amenity.py
@@ -3,13 +3,20 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import association_table
+import models
 
 
 class Amenity(BaseModel, Base):
     """Amenity model of hbnb project"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        "Place",
-        secondary=association_table,)
+    
+    # For DBStorage: Many-to-Many relationship with Place
+    if models.storage_t == "db":
+        place_amenities = relationship("Place", secondary="place_amenity", back_populates="amenities")
+    else:
+        place_amenities = []
+    
+    def __init__(self, *args, **kwargs):
+        """Initializes Amenity"""
+        super().__init__(*args, **kwargs)
