f3f950fb102c49ada7a511ef62b890ceadd064e3
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:33:39 +0200

    added

--- models/amenity.py
+++ models/amenity.py
@@ -6,10 +6,14 @@ Contains the Amenity class
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+    
+    # For DBStorage: relationship with Place using place_amenity as secondary table
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship("Place", secondary="place_amenity", viewonly=False, back_populates="amenities")
