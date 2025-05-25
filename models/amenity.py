ac67ed79a25d3ed43c8b31819e5403210ebfd7e2
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:38:47 +0200

    add

--- models/amenity.py
+++ models/amenity.py
@@ -15,5 +15,4 @@ class Amenity(BaseModel, Base):
     name = Column(String(128), nullable=False)
     
     # For DBStorage: relationship with Place using place_amenity as secondary table
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship("Place", secondary="place_amenity", viewonly=False, back_populates="amenities")
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
