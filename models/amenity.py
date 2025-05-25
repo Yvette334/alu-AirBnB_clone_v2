a687114ea9f5d2bbf7e309195c5cfb56b7375b72
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:07:26 +0200

    add

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,11 @@
 Contains the Amenity class
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Representation of Amenity"""
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
