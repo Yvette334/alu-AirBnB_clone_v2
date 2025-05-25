757d9ba84368520d623c1672896025304e4c5b56
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 20:21:58 +0200

    add

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ Base = declarative_base()
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    
+
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
@@ -24,8 +24,9 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    if key in ("created_at", "updated_at"):
+                        setattr(self, key, datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, value)
         else:
@@ -35,8 +36,8 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__,
-                                         self.id, self.__dict__)
+        return "[{:s}] ({:s}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates the attribute 'updated_at' with the current datetime"""
@@ -48,9 +49,11 @@ class BaseModel:
         """Returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
+            new_dict["created_at"] = new_dict["created_at"].strftime(
+                "%Y-%m-%dT%H:%M:%S.%f")
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(
+                "%Y-%m-%dT%H:%M:%S.%f")
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
