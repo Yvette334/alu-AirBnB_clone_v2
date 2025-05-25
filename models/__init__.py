8cfdfa5d1e1c9da8fb98e16376a3fdf9834539c3
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:06:21 +0200

    add

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,14 @@
 Initialize the models package
 """
 
-from models.engine.file_storage import FileStorage
+import os
 
-storage = FileStorage()
+storage_t = os.getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
