fdf51d1b49cfed892839ada7b8592dbbdaae35bb
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:31:07 +0200

    add

--- models/amenity.py
+++ models/amenity.py
@@ -5,9 +5,11 @@ Contains the Amenity class
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
