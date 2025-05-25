162ba34de0ad02934a074212aaa74f7afdd396a4
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:04:55 +0200

    added

--- models/state.py
+++ models/state.py
@@ -3,9 +3,25 @@
 Contains the State class
 """
 
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """Representation of state"""
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """FileStorage relationship between State and City"""
+        from models.city import City
+        city_list = []
+        all_cities = models.storage.all(City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
