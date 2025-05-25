0883729ba8bc50ce568b6680040d8ac44ba3309c
Yvette334 <y.muhoracye@alustudent.com>
2025-05-25 10:05:38 +0200

    added

--- models/city.py
+++ models/city.py
@@ -3,10 +3,12 @@
 Contains the City class
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """Representation of city"""
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
