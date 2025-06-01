b8850e6b4dfde157c7046cbd2a46e85b5c1440de
Yvette334 <y.muhoracye@alustudent.com>
2025-06-01 16:48:27 +0200

    add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,40 @@
 #!/usr/bin/python3
-"""
-Fabric script to distribute an archive to web servers
-"""
-
+""" Function that compress a folder """
+from datetime import datetime
 from fabric.api import *
-from os.path import exists
+import shlex
 import os
 
-env.hosts = ['54.175.105.22', '54.175.110.39']
-env.user = 'ubuntu' 
+
+env.hosts = ["54.175.105.22", "54.175.110.39"]
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    """
-    if not exists(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
-
     try:
-        # Upload the archive to /tmp/ directory of web server
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-
-        # Get archive filename without extension
-        archive_filename = os.path.basename(archive_path)
-        archive_name = archive_filename.split('.')[0]
-
-        # Create target directory
-        releases_path = "/data/web_static/releases/{}/".format(archive_name)
         run("mkdir -p {}".format(releases_path))
-
-        # Uncompress archive to target directory
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, releases_path))
-
-        # Delete archive from web server
-        run("rm /tmp/{}".format(archive_filename))
-
-        # Move contents to proper location
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
         run("mv {}web_static/* {}".format(releases_path, releases_path))
         run("rm -rf {}web_static".format(releases_path))
-
-        # Delete existing symbolic link
         run("rm -rf /data/web_static/current")
-
-        # Create new symbolic link
         run("ln -s {} /data/web_static/current".format(releases_path))
-
         print("New version deployed!")
         return True
-
-    except Exception as e:
-        print("Deployment failed:", str(e))
+    except:
         return False
